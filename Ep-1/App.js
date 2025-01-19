f3bf522847afc7466e74f86bfc233e7268ece410
Om Patel <omupatel22@gmail.com>
2025-01-19 15:43:54 +0530

    Episode-01

---
+++ Ep-1/App.js
@@ -0,0 +1,19 @@
+const parent=React.createElement("div",{id:"parent"},[
+    React.createElement("div",{id:"c1"},[
+        React.createElement("h1",{},"h1"),
+        React.createElement("h2",{},"h2")
+    ]),
+    React.createElement("div",{id:"c2"},[
+        React.createElement("h1",{},"h1"),
+        React.createElement("h2",{},"h2")
+    ])
+])
+
+// const heading=React.createElement(
+//     "h1",
+//     {id:"heading", xyz:"abc"},
+//     "hello from react"
+// );
+console.log(parent);
+const root=ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
